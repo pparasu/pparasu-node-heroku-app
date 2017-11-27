b7bb3da1bc51a246f94c06df31f982b8b654b33a
Parasuraman <apple@Apples-MacBook-Pro.local>
2017-11-27 09:58:44 +0530

    V2.1

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+app.set('port', (process.env.PORT || 5000))
+
+const REQUIRE_AUTH = true
+const AUTH_TOKEN = 'an-example-token'
+
+app.get('/', function (req, res) {
+  //res.send('Use the /webhook endpoint.')
+})
+app.get('/echo', function (req, res) {
+  //res.send('You must POST your request')
+
+  const userName = req.query.userName
+  //var userName = req.body.result.parameters['given-name']
+  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+  //var userName = req.parms('id')
+  //console.log ('User Name '+userName+' webhookReply '+webhookReply)
+  //console.log ('User Name '+userName)
+  res.send('<center><h1>'+webhookReply+'</h1')
+})
+
+app.post('/echo', function (req, res) {
+  // we expect to receive JSON data from api.ai here.
+  // the payload is stored on req.body
+  console.log(req.body)
+
+
+  const userName = req.query.userName
+  //var userName = req.body.result.parameters['given-name']
+  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+
+  // we have a simple authentication
+  // if (REQUIRE_AUTH) {
+  //   if (req.headers['auth-token'] !== AUTH_TOKEN) {
+  //     return res.status(401).send('Unauthorized')
+  //   }
+  // }
+
+  // and some validation too
+  //if (!req.body || !req.body.result || !req.body.result.parameters) {
+  //  return res.status(400).send('Bad Request')
+  //}
+
+  // the value of Action from api.ai is stored in req.body.result.action
+//  console.log('* Received action -- %s', req.body.result.action)
+
+  // parameters are stored in req.body.result.parameters
+//  var userName = req.body.result.parameters['given-name']
+//  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+
+  // the most basic response
+  res.status(200).json({
+    source: 'echo',
+    speech: webhookReply,
+    displayText: webhookReply
+  })
+})
+
+app.listen(app.get('port'), function () {
+  console.log('* Webhook service is listening on port:' + app.get('port'))
+})
