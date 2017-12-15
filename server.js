2369e3d17adc2861489c4e20b1243ca6b463c333
Parasuraman <apple@Apples-MacBook-Pro.local>
2017-12-15 14:28:47 +0530

    V2.4

--- server.js
+++ server.js
@@ -8,31 +8,13 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/intro', function(req, res){
-  var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.userName ? req.body.result.parameters : "Seems like some problem. Try again"
-  return res.json({
-    speech : speech,
-    displayText : speech,
-    source : 'webhook-echo-sample'
-  });
-
+app.post('/intro', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
 });
-
-
-
-
-app.get('/intro', function (req, res) {
-  const userName = req.query.userName
-  console.log(req.hostname)
-  console.log('URL '+req.originalUrl)
-  console.log('Query '+req.query)
-  console.log(req.body)
-  //var userName = req.body.result.parameters['userName']
-  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
-
-  res.send(webhookReply)
-})
-
-
 
 app.listen(process.env.PORT || 8080, () => console.log('All is OK'))
